55f62f7faf136a8dd39eba96f20d2bbdc25fd183
arfin-arif <arifmtbd@gmail.com>
2022-10-27 11:15:43 +0600

    all categories api created

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors())
+const courses = require('./data/Courses/courses.json')
+const categories = require('./data/Categories/Categories.json')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+// all categories
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
+
+
+
+app.listen(port, () => {
+    console.log(` app running on port ${port}`)
+})
